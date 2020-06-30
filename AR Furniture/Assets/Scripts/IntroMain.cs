998386f6ef5e367760ec9c06e73e09461e3f28f5
Aakiliqbal <aakiliqbal645@gmail.com>
2020-06-30 22:12:57 +0530

    AR Furniture App

---
+++ AR Furniture/Assets/Scripts/IntroMain.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroMain : MonoBehaviour
+{
+    public void startApp()
+    {
+        SceneManager.LoadScene("Arfoundation");
+    }
+    public void QuitApp()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+    
+    public void IntroMenu()
+    {
+        SceneManager.LoadScene("Intro");
+    }
+}
---
+++ AR Furniture/Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject Chair;
+    public GameObject Table;
+
+    private PlacementIndicator placementIndicator;
+
+
+    void Start()
+    {
+        placementIndicator = FindObjectOfType<PlacementIndicator>();
+
+    }
+
+
+    public void SpawnChair() {
+
+        GameObject obj = Instantiate(Chair,
+                placementIndicator.transform.position, placementIndicator.transform.rotation);
+
+    }
+   
+   
+    public void SpawnTable() {
+
+        GameObject obj = Instantiate(Table,
+                placementIndicator.transform.position, placementIndicator.transform.rotation);
+
+    }    
+    
+   
+}
